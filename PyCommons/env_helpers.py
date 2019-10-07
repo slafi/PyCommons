c063e8ecc3fcad9906ff1a330724036a2989f05c
slafi <lafi_saber@yahoo.fr>
2019-10-06 20:35:29 -0400

    Initial release

--- PyCommons/env_helpers.py
+++ PyCommons/env_helpers.py
@@ -1,4 +1,6 @@
 import pkg_resources
+import pkgutil
+import os
 from subprocess import call
 
 
@@ -11,3 +13,21 @@ def update_packages():
     packages = [dist.project_name for dist in pkg_resources.working_set]
     call("pip install --upgrade " + ' '.join(packages), shell=True)
 
+
+
+def list_package_modules(package_name):
+    """returns the list of a package modules
+       :param package_name: a package folder name
+       :return: a list of modules if succes, None if an exception arises
+    """
+    directory = str(package_name)
+    if not (os.path.isdir(directory) and os.path.exists(directory)):
+        print(f"Invalid package {package_name}.")
+        return None
+
+    search_path = [directory]
+    all_modules = [x[1] for x in pkgutil.iter_modules(path=search_path)]
+    print(all_modules)
+
+    return all_modules
+	
--- Readme.md
+++ Readme.md
@@ -15,6 +15,7 @@ The following table shows the available helper functions:
 | `check_mac_address`      | Checks if a string matches the pattern of a MAC address | patterns.py |
 | `infer_type`      | Matches the input with predefined patterns to determine its data type | patterns.py |
 | `update_packages`      | Lists the current Python environment packages and calls the PIP module to upgrade them sequentially | env_helpers.py |
+| `list_package_modules`      | Lists the modules of a given package | env_helpers.py |
 
 ## Available Helper Classes
 
